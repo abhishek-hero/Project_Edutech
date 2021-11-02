99e18cbfb403236ae3c08224d73d0f38a32f0a5a
Abhishek M <abhimukade2000@gmail.com>
2021-11-02 13:21:01 +0530

    created folder structure

---
+++ project-edutech/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Router, Switch, Route } from 'react-router-dom'
+
+
+// Note: If you want to use bootstrap then import it like 
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// Path may change according to file
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
