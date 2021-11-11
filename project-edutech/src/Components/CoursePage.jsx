ba5868c830fb6a7e86903e7bf2feed1060787399
AnandaGouda <ananda_fw10_072@masai.school>
2021-11-11 06:24:49 +0530

    addded some more file

---
+++ project-edutech/src/Components/CoursePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./CoursePage.css"
+import {Button} from "./Button"
+import { Overview } from './Overview'
+import {BookDemo} from "./BookDemo"
+import { Home } from './Home'
+
+function CoursePage() {
+    return (
+        <div>
+            <div className="top-section">
+                <div className="buy-now-btn">
+                <Button content="Buy Now"/>
+                </div>
+            </div>
+            <div className="demo-video">
+
+            </div>
+            <div className="overview-book">
+               <a href="">Over View</a>
+               <a href="">Book a Demo</a> 
+            </div>
+            <div className="book-demo">
+                <BookDemo/>
+            </div>
+        </div>
+    )
+}
+
+export  {CoursePage}
---
+++ project-edutech/src/Components/InternshipDetails.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./GigsDetail.css"
+import {Footer} from "./Footer"
+
+function InternshipDetails() {
+    return (
+        <div>
+          <div className="gigs"></div>
+          <div className="gigs-div">
+              <div className="intern-name">Internship Name</div>
+              <div className="intern-title">About the Internship</div>
+              <div className="gigs-detail">
+               <p> Lorem ipsum dolor sit amet, consectetur elit.<br/>
+                 Proin finibus vitae turpis non iaculis. 
+                 Quisque<br/> nec erat eu diam egestas dapibus nec
+                  non ante<br/> adipiscing Vivamus molestie sodales congue.<br/>
+                   Aenean tincidunt erat non nulla iaculis, non<br/> pretium odio feugiat.</p>
+              </div>
+              <div className="calendar">
+              <span>
+              <img src="/Images/calendar.png" alt="calender"/>
+              <p>Last date to apply:</p>
+              <div className="span-btn"><p>01-09-21</p></div>
+             </span>
+              <span>
+              <img src="/Images/calendar.png" alt="calender"/>  
+              <p>From:</p>
+              <div className="span-btn"><p>01-09-21</p></div>
+              <p>To:</p>
+              <div className="span-btn"><p>01-10-21</p></div>
+              </span>
+              <span>
+              <img src="/Images/calendar.png" alt="calender"/>  
+              <p>From:</p>
+              <div className="span-btn"><p>12:00PM</p></div>
+              <p>To:</p>
+              <div className="span-btn"><p>04:00PM</p></div>
+              </span>
+              <span>
+                <img src="/Images/rupee.png" alt="rupee"/>
+                <div className="span-btn"><p>500</p></div>
+              </span>
+              <div className="app-btn">
+                 <button>Apply</button>
+                 <button>Chat</button> 
+              </div>
+              </div>
+             
+              </div>
+              <Footer/>
+
+        </div>
+    )
+}
+
+export  {InternshipDetails}
