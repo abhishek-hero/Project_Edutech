6b94ed51f0c2577c316621ed1e6189aabfa4ad5a
Manasvi Sharma <manasvi.manu94@gmail.com>
2021-11-13 01:21:06 +0530

    Routing

--- project-edutech/src/Components/Home/LiveCarouselTop.jsx
+++ project-edutech/src/Components/Home/LiveCarouselTop.jsx
@@ -1,148 +1,162 @@
-import React from 'react'
-import './Styles/LiveCarouselTop.css'
+import React from "react";
+import "./Styles/LiveCarouselTop.css";
+import { Link } from "react-router-dom";
 
-export const LiveCarouselTop = ({ title, background, background1, background2, background3, pathout }) => {
-    return (
+export const LiveCarouselTop = ({
+  title,
+  background,
+  background1,
+  background2,
+  background3,
+  pathout,
+}) => {
+  return (
+    <>
+      <div className="carousel_heading">
+        <span className="carousel_title">{title}</span>
 
-        <>
-            <div className="carousel_heading">
-                <span className='carousel_title'>{title}</span>
-                <span className='carousel_link'>See all</span>
+        <Link to="/free-live-demo">
+          <span className="carousel_link">See all</span>
+        </Link>
+      </div>
+
+      <div className="carousel_home">
+        <span>
+          <div
+            className="Livecarousel_div"
+            style={{ backgroundImage: `url(${background1})` }}
+          >
+            <div className="overlay">
+              <div className="card_btn_top">
+                <p>6 pm Tuesday</p>
+                <span>
+                  <button>LIVE</button>
+                </span>
+              </div>
+
+              <div className="card_btn_middle">
+                <p>Mostly used cameras by experts</p>
+              </div>
+
+              <div className="card_btn_bottom">
+                <img src="/Images/cameraMan.png" alt="" />
+                <span>
+                  <strong>Ashwath Ram</strong>
+                  <small>Wild Life Photographer</small>
+                </span>
+              </div>
             </div>
-
-            <div className="carousel_home">
-
+          </div>
+        </span>
+        <span>
+          <div
+            className="Livecarousel_div"
+            style={{ backgroundImage: `url(${background2})` }}
+          >
+            <div className="overlay">
+              <div className="card_btn_top">
+                <p>6 pm Tuesday</p>
                 <span>
-
-                    <div className="Livecarousel_div" style={{ backgroundImage: `url(${background1})` }}>
-                        <div className='overlay'>
-                            <div className='card_btn_top'>
-                                <p>6 pm Tuesday</p>
-                                <span>
-                                    <button>LIVE</button>
-                                </span>
-                            </div>
-
-                            <div className='card_btn_middle'>
-                                <p>Mostly used cameras by experts</p>
-                            </div>
-
-                            <div className='card_btn_bottom'>
-                                <img src="/Images/cameraMan.png" alt="" />
-                                <span>
-                                    <strong>Ashwath Ram</strong>
-                                    <small>Wild Life Photographer</small>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-
+                  <button>LIVE</button>
                 </span>
+              </div>
+
+              <div className="card_btn_middle">
+                <p>Mostly used cameras by experts</p>
+              </div>
+
+              <div className="card_btn_bottom">
+                <img src="/Images/cameraMan.png" alt="" />
                 <span>
-
-                    <div className="Livecarousel_div" style={{ backgroundImage: `url(${background2})` }}>
-                        <div className='overlay'>
-                            <div className='card_btn_top'>
-                                <p>6 pm Tuesday</p>
-                                <span>
-                                    <button>LIVE</button>
-                                </span>
-                            </div>
-
-                            <div className='card_btn_middle'>
-                                <p>Mostly used cameras by experts</p>
-                            </div>
-
-                            <div className='card_btn_bottom'>
-                                <img src="/Images/cameraMan.png" alt="" />
-                                <span>
-                                    <strong>Ashwath Ram</strong>
-                                    <small>Wild Life Photographer</small>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
+                  <strong>Ashwath Ram</strong>
+                  <small>Wild Life Photographer</small>
                 </span>
-                <span>
-
-                    <div className="Livecarousel_div" style={{ backgroundImage: `url(${background3})` }}>
-                        <div className='overlay'>
-                            <div className='card_btn_top'>
-                                <p>6 pm Tuesday</p>
-                                <span>
-                                    <button>LIVE</button>
-                                </span>
-                            </div>
-
-                            <div className='card_btn_middle'>
-                                <p>Mostly used cameras by experts</p>
-                            </div>
-
-                            <div className='card_btn_bottom'>
-                                <img src="/Images/cameraMan.png" alt="" />
-                                <span>
-                                    <strong>Ashwath Ram</strong>
-                                    <small>Wild Life Photographer</small>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-
-                </span>
-                <span>
-
-                    <div className="Livecarousel_div" style={{ backgroundImage: `url(${background1})` }}>
-                        <div className='overlay'>
-                            <div className='card_btn_top'>
-                                <p>6 pm Tuesday</p>
-                                <span>
-                                    <button>LIVE</button>
-                                </span>
-                            </div>
-
-                            <div className='card_btn_middle'>
-                                <p>Mostly used cameras by experts</p>
-                            </div>
-
-                            <div className='card_btn_bottom'>
-                                <img src="/Images/cameraMan.png" alt="" />
-                                <span>
-                                    <strong>Ashwath Ram</strong>
-                                    <small>Wild Life Photographer</small>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-
-                </span>
-                <span>
-
-                    <div className="Livecarousel_div" style={{ backgroundImage: `url(${background1})` }}>
-                        <div className='overlay'>
-                            <div className='card_btn_top'>
-                                <p>6 pm Tuesday</p>
-                                <span>
-                                    <button>LIVE</button>
-                                </span>
-                            </div>
-
-                            <div className='card_btn_middle'>
-                                <p>Mostly used cameras by experts</p>
-                            </div>
-
-                            <div className='card_btn_bottom'>
-                                <img src="/Images/cameraMan.png" alt="" />
-                                <span>
-                                    <strong>Ashwath Ram</strong>
-                                    <small>Wild Life Photographer</small>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-
-                </span>
+              </div>
             </div>
+          </div>
+        </span>
+        <span>
+          <div
+            className="Livecarousel_div"
+            style={{ backgroundImage: `url(${background3})` }}
+          >
+            <div className="overlay">
+              <div className="card_btn_top">
+                <p>6 pm Tuesday</p>
+                <span>
+                  <button>LIVE</button>
+                </span>
+              </div>
 
-        </>
-    )
-}
+              <div className="card_btn_middle">
+                <p>Mostly used cameras by experts</p>
+              </div>
+
+              <div className="card_btn_bottom">
+                <img src="/Images/cameraMan.png" alt="" />
+                <span>
+                  <strong>Ashwath Ram</strong>
+                  <small>Wild Life Photographer</small>
+                </span>
+              </div>
+            </div>
+          </div>
+        </span>
+        <span>
+          <div
+            className="Livecarousel_div"
+            style={{ backgroundImage: `url(${background1})` }}
+          >
+            <div className="overlay">
+              <div className="card_btn_top">
+                <p>6 pm Tuesday</p>
+                <span>
+                  <button>LIVE</button>
+                </span>
+              </div>
+
+              <div className="card_btn_middle">
+                <p>Mostly used cameras by experts</p>
+              </div>
+
+              <div className="card_btn_bottom">
+                <img src="/Images/cameraMan.png" alt="" />
+                <span>
+                  <strong>Ashwath Ram</strong>
+                  <small>Wild Life Photographer</small>
+                </span>
+              </div>
+            </div>
+          </div>
+        </span>
+        <span>
+          <div
+            className="Livecarousel_div"
+            style={{ backgroundImage: `url(${background1})` }}
+          >
+            <div className="overlay">
+              <div className="card_btn_top">
+                <p>6 pm Tuesday</p>
+                <span>
+                  <button>LIVE</button>
+                </span>
+              </div>
+
+              <div className="card_btn_middle">
+                <p>Mostly used cameras by experts</p>
+              </div>
+
+              <div className="card_btn_bottom">
+                <img src="/Images/cameraMan.png" alt="" />
+                <span>
+                  <strong>Ashwath Ram</strong>
+                  <small>Wild Life Photographer</small>
+                </span>
+              </div>
+            </div>
+          </div>
+        </span>
+      </div>
+    </>
+  );
+};
