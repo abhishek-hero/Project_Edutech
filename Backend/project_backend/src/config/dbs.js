9510aa49c474228e4f8b8b49d69386156f1700b8
Abhishek Mukade <87421921+abhishek-hero@users.noreply.github.com>
2021-11-12 15:04:49 +0530

    Delete Backend/project_backend directory

--- Backend/project_backend/src/config/dbs.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = ()=>{
-    mongoose.connect("mongodb+srv://abhishek-hero:mahabharat@project-edutech.uyk3o.mongodb.net/project-edutech")
-}
-
-module.exports = connect
