5b3a613e08cc64c2d8391eb6474589ff8bcbd70e
Abhishek M <abhimukade2000@gmail.com>
2021-11-13 17:50:31 +0530

    completed project

--- project-edutech/src/Components/Home/Banner.jsx
+++ project-edutech/src/Components/Home/Banner.jsx
@@ -4,7 +4,7 @@ import './Styles/Banner.css'
 export const Banner = () => {
     return (
         <div className='home_banner'>
-
+            <img src="https://source.unsplash.com/1600x900/?javascript" alt="" />
         </div>
     )
 }
--- project-edutech/src/Components/Mentor/MentorPage.jsx
+++ project-edutech/src/Components/Mentor/MentorPage.jsx
@@ -5,26 +5,28 @@ import { Footer } from '../Home/Footer'
 
 export const MentorPage = () => {
     return (
-        <div className="mentorPage_main_div">
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
-            <Mentor></Mentor>
+        <div>
+            <div className="main_div_mentor">
+                <img src="/Icons/BackIcon.svg" alt="" />
+                <p>Who are your mentors?</p>
+            </div>
+            <div className="mentorPage_main_div">
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
+                <Mentor></Mentor>
 
+                <div className="space_div_mentor"></div>
+
+            </div>
             <Footer></Footer>
         </div>
     );
