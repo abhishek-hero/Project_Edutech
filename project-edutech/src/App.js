eef8798281ef9cc917aa804d573a8e5272952192
kapil <kapilmogre1998@gmail.com>
2021-11-13 10:32:26 +0530

    created free courses

--- project-edutech/src/App.js
+++ project-edutech/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import { Home } from './Components/Home'
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { CourseLesson } from './Components/FreeCourses/CourseLesson';
+import { FreeCourses } from './Components/FreeCourses/FreeCourse';
 
 
 function App() {
   return (
-    <div className="App">
-
-      <Home />
-
-    </div>
-   
+   <div className="App">
+     
+   </div>
   );
 }
 
