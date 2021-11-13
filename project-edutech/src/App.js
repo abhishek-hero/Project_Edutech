14342dd6f1c4e554a983267696f3c7ee55566a17
Abhishek M <abhimukade2000@gmail.com>
2021-11-13 10:51:36 +0530

    changed routes in app.js

--- project-edutech/src/App.js
+++ project-edutech/src/App.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Home } from './Components/Home/Home'
-import { MentorPage } from './Components/Mentor/MentorPage'
-// import { LoginPage } from './Components/Login/LoginPage'
-// import { RegisterNow } from './Components/Register/RegisterNow'
-// import { Carousel } from './Carousel/Carousel'
-// import { OtpPage } from './Components/Register/OtpPage'
-import SignUp from './Components/Register/SignUp'
-import Otp from './Components/Register/Otp'
-=======
 import './App.css';
 import { Home } from './Components/Home'
 
->>>>>>> 6f271912acd7e986c56f3b5bb73016e68913952a
 
 
 function App() {
   return (
-<<<<<<< HEAD
 
     <Router>
       <div className="App">
@@ -49,14 +35,6 @@ function App() {
 
       </div>
     </Router>
-=======
-    <div className="App">
-
-      <Home />
-
-    </div>
-   
->>>>>>> 6f271912acd7e986c56f3b5bb73016e68913952a
   );
 }
 
